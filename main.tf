f8b01bc67dc11977056979d5a251fe3736df2214
dcrdevops <dcrdevops24@gmail.com>
2026-05-30 11:27:23 +0530

    updated identifier

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_versioning" "digi-s3-version" {
 data "aws_iam_policy_document" "digi-s3-policy-statement"{
   statement {
     principals {
-      identifiers = var.account
+      identifiers = [var.account]
       type = "AWS"
     }
     actions = [
